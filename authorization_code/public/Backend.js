d45b23937a2b64471e16845e91131c50c3bef292
MuradAwad <Murad.bummos@gmail.com>
2016-07-07 19:03:35 -0700

    Basic layout for Spotify Web API

---
+++ authorization_code/public/Backend.js
@@ -0,0 +1,72 @@
+﻿/**
+ * This is an example of a basic node.js script that performs
+ * the Authorization Code oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
+ */
+
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = 'c4a76c855c7146ffbd9f504995d0ebe8'; // Your client id
+var client_secret = '815d7864e93d470dbfd4ba517f09c4de'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+var Genre = document.getElementById("genre");
+var Song = document.getElementById("song");
+(function () {
+    var audio = new Audio();
+
+    function searchTracks(query) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            data: {
+                q: query,
+                type: 'track'
+            },
+            success: function (response) {
+                if (response.tracks.items.length) {
+                    var track = response.tracks.items[0];
+                    audio.src = track.preview_url;
+                    audio.play();
+                    communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+                }
+            }
+        });
+    }
+
+    function playSong(songName, artistName) {
+        var query = songName;
+        if (artistName) {
+            query += ' artist:' + artistName;
+        }
+
+        searchTracks(query);
+    }
+
+});
---
+++ authorization_code/public/search.js
@@ -0,0 +1,12 @@
+﻿var Genre = document.getElementById("genre");
+var Song = document.getElementById("song");
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    searchTracks(Song.value.toString());
+},
+    false
+    );
+function run() {
+    searchTracks(Song.value);
+    alert("HEY");
+}
